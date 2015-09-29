d13a9893ef3b27917e6e2591cd8bc7929fcc89b8
iamjohnandrews <johnnydrews@gmail.com>
2015-09-28 23:01:47 -0400

    onboarding page index is inconsistent - add auto layout

--- TOP5/ViewController.swift
+++ TOP5/ViewController.swift
@@ -10,19 +10,19 @@ import UIKit
 
 class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
     
-    var pageViewController: UIPageViewController!
+    public var pageViewController: UIPageViewController!
     var pageTitles: [String]!
+    public var genericLabel = UILabel()
+    var index = 0
     
     @IBOutlet weak var facebookLoginButton: UIButton!
     @IBOutlet weak var emailLoginButton: UIButton!
      
     override func viewDidLoad() {
         super.viewDidLoad()
-        onboardingPage1UISetUp()
         pageTitles = ["page1", "page2", "page3", "page4", "page5"]
 
         pageViewController = storyboard?.instantiateViewControllerWithIdentifier("PageViewController") as! UIPageViewController
-
         pageViewController.dataSource = self
 
         let pageController = UIPageControl.appearance()
@@ -34,12 +34,60 @@ class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageVi
         let viewControllers = NSArray(object: startVC)
 
         pageViewController.setViewControllers(viewControllers as! [UIViewController], direction: .Forward, animated: true, completion: nil)
-        pageViewController.view.frame = CGRectMake(0, 30, view.frame.width, view.frame.height - 180)
+        pageViewController.view.frame = CGRectMake(0, view.frame.height/2 - 150, view.frame.width, view.frame.height/2 + 60)
         addChildViewController(pageViewController)
         view.addSubview(pageViewController.view)
         pageViewController.didMoveToParentViewController(self)
+
+        setUpLogoTextOnPage1(index)
+        emailLoginButton.layer.borderColor = UIColor.blackColor().CGColor
+        emailLoginButton.layer.borderWidth = 1
     }
 
+    // MARK: Onboarding
+    private func setUpLogoTextOnPage1(pageIndex: Int) {
+        genericLabel.frame = CGRectMake(50, 50, 400, 200)
+        genericLabel.textAlignment = NSTextAlignment.Center
+        genericLabel.font = genericLabel.font.fontWithSize(80)
+        
+        genericLabel.center = CGPointMake(view.center.x, view.center.y - 100)
+        
+        let baseString = "TopFIVE" as NSString
+        let attributedString = NSMutableAttributedString(string: baseString as String)
+        
+        let thinFont = UIFont(name: "HelveticaNeue-Thin", size: 80)!
+        let thinAttribute = [NSFontAttributeName: thinFont]
+        let boldAttribute = [NSFontAttributeName : UIFont.boldSystemFontOfSize(80)]
+        attributedString.addAttributes(boldAttribute, range: baseString.rangeOfString("FIVE"))
+        attributedString.addAttributes(thinAttribute, range: baseString.rangeOfString("Top"))
+        
+        genericLabel.attributedText = attributedString
+        
+        view.addSubview(genericLabel)
+    }
+    
+    private func setUpOnboardingNumbers(pageIndex: Int) {
+        genericLabel.center = CGPointMake(view.center.x, view.center.y - 100)
+        
+        let numberString = "1 2 3 4" as NSString
+        let numberAttributedString = NSMutableAttributedString(string: numberString as String)
+        let greyColorAttribute = [NSForegroundColorAttributeName: UIColor.lightGrayColor()]
+        let boldAttribute = [NSFontAttributeName : UIFont.boldSystemFontOfSize(80)]
+        
+        if pageIndex == 1 {
+            numberAttributedString.addAttributes(greyColorAttribute, range: numberString.rangeOfString("2 3 4"))
+        } else if pageIndex == 2 {
+            numberAttributedString.addAttributes(greyColorAttribute, range: numberString.rangeOfString("1"))
+            numberAttributedString.addAttributes(greyColorAttribute, range: numberString.rangeOfString("3 4"))
+        } else if pageIndex == 3 {
+            numberAttributedString.addAttributes(greyColorAttribute, range: numberString.rangeOfString("1 2"))
+            numberAttributedString.addAttributes(greyColorAttribute, range: numberString.rangeOfString("4"))
+        } else if pageIndex == 4 {
+            numberAttributedString.addAttributes(greyColorAttribute, range: numberString.rangeOfString("1 2 3"))
+        }
+        numberAttributedString.addAttributes(boldAttribute, range: numberString.rangeOfString(numberString as String))
+                genericLabel.attributedText = numberAttributedString
+    }
     
     @IBAction func emailLoginButtonAction(sender: AnyObject) {
     }
@@ -48,15 +96,6 @@ class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageVi
     }
    
     // MARK: Onboarding Views UI Set Up
-    private func onboardingPage1UISetUp () {
-        let continueButton = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: "removePageViewController:")
-        continueButton.title = "Skip"
-        self.navigationItem.rightBarButtonItem = continueButton
-
-        emailLoginButton.layer.borderColor = UIColor.blackColor().CGColor
-        emailLoginButton.layer.borderWidth = 1
-        
-    }
 
     func removePageViewController(sender: AnyObject) {
         navigationController?.popViewControllerAnimated(true)
@@ -71,7 +110,6 @@ class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageVi
 
         let vc = storyboard?.instantiateViewControllerWithIdentifier("ContentViewController") as! WelcomeViewController
 
-        vc.titleText = pageTitles[index] as String
         vc.pageIndex = index
 
         return vc
@@ -81,10 +119,13 @@ class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageVi
     
     func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
         let vc = viewController as! WelcomeViewController // could have this be a different VC based on index and it will display a differeent vc
-        var index = vc.pageIndex as Int
+        index = vc.pageIndex as Int
 
         if index == 0 || index == NSNotFound {
+            setUpLogoTextOnPage1(index)
             return nil
+        } else {
+            setUpOnboardingNumbers(index)
         }
 
         index--
@@ -93,17 +134,17 @@ class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageVi
     
     func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
         let vc = viewController as! WelcomeViewController
-        var index = vc.pageIndex as Int
+        index = vc.pageIndex as Int
 
-        if index == NSNotFound {
+        if index == NSNotFound || index == pageTitles.count {
             return nil
+        }
+        
+        if index > 0 {
+            setUpOnboardingNumbers(index)
         }
 
         index++
-
-        if index == pageTitles.count {
-            return nil
-        }
 
         print("ViewController Hash=\(viewController.hash) and Index =\(index)")
 
--- TOP5/WelcomeViewController.swift
+++ TOP5/WelcomeViewController.swift
@@ -12,91 +12,80 @@ import UIKit
 public class WelcomeViewController: UIViewController {
 
     public var pageIndex = 0
-    public var titleText = ""
-    var genericLabel = UILabel()
+    
+    private let pageBold1 = "YOURSELF"
+    private let pageNormal1 = "Tell us about"
+    private var pageText1 : String {
+        get {
+            return pageNormal1 + " " + pageBold1
+        }
+    }
+    private let pageBold2 = "YOUR FAVORITES"
+    private let pageNormal2 = "Tell us about"
+    private var pageText2 : String {
+        get {
+            return pageNormal2 + " " + pageBold2
+        }
+    }
+    private let pageBold3 = "OTHER'S FAVORITES"
+    private let pageNormal3 = "Search for "
+    private var pageText3 : String {
+        get {
+            return pageNormal3 + " " + pageBold3
+        }
+    }
+    private let pageBold4 = "YOUR NEW FAVORITES"
+    private let pageNormal4 = "Discover"
+    private var pageText4 : String {
+        get {
+            return pageNormal4 + " " + pageBold4
+        }
+    }
+
     
     override public func viewDidLoad() {
         super.viewDidLoad()
         NSLog("pageIndex =\(pageIndex)")
 
-        if pageIndex == 0 {
-            setUpLabel(false)
-            setUpLogoTextOnPage1()
-        } else {
-            setUpLabel(true)
-            setupTextView()
-        }
-    }
-
-    private func setUpLabel(withBottomBorder: Bool) {
-        genericLabel.frame = CGRectMake(50, 50, 400, 200)
-        genericLabel.textAlignment = NSTextAlignment.Center
-        genericLabel.font = genericLabel.font.fontWithSize(80)
-        view.addSubview(genericLabel)
-
-        if withBottomBorder == true {
-            setUpOnboardingNumbers()
-            let bottomBorder = CALayer()
-            bottomBorder.borderColor = UIColor.blackColor().CGColor
-            bottomBorder.borderWidth = 4
-            bottomBorder.frame = CGRectMake(genericLabel.frame.origin.x, genericLabel.frame.origin.y + genericLabel.frame.size.height, CGRectGetWidth(genericLabel.frame), 2)
-        }
-    }
-
-    private func setupTextView() {
-        let textView = UITextView(frame: CGRectMake(genericLabel.frame.origin.x, genericLabel.frame.origin.y + genericLabel.frame.size.height + 30, genericLabel.frame.size.width, 180))
-        view.addSubview(textView)
-
-        var topBaseString = "Tell us about YOURSELF" as NSString
-        var topAttributedString = NSMutableAttributedString(string: topBaseString as String)
-        let regularTopText = [NSFontAttributeName : UIFont.systemFontOfSize(20)]
-        let boldAttribute = [NSFontAttributeName : UIFont.boldSystemFontOfSize(20)]
-        topAttributedString.addAttributes(boldAttribute, range: topBaseString.rangeOfString("YOURSELF"))
-        topAttributedString.addAttributes(regularTopText, range: topBaseString.rangeOfString("Tell us about"))
-
-        textView.attributedText = topAttributedString;
-
-        var bottomString = "The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence."
-
-
-    }
-
-    private func setUpLogoTextOnPage1() {
-        genericLabel.center = CGPointMake(view.center.x, view.center.y - 100)
-
-        let baseString = "TopFIVE" as NSString
-        let attributedString = NSMutableAttributedString(string: baseString as String)
-
-        let thinFont = UIFont(name: "HelveticaNeue-Thin", size: 80)!
-        let thinAttribute = [NSFontAttributeName: thinFont]
-        let boldAttribute = [NSFontAttributeName : UIFont.boldSystemFontOfSize(80)]
-        attributedString.addAttributes(boldAttribute, range: baseString.rangeOfString("FIVE"))
-        attributedString.addAttributes(thinAttribute, range: baseString.rangeOfString("Top"))
-
-        genericLabel.attributedText = attributedString
-    }
-
-    private func setUpOnboardingNumbers() {
-        genericLabel.center = CGPointMake(view.center.x, view.center.y - 100)
-
-        let numberString = "1 2 3 4" as NSString
-        let numberAttributedString = NSMutableAttributedString(string: numberString as String)
-        let greyColorAttribute = [NSForegroundColorAttributeName: UIColor.lightGrayColor()]
-        let boldAttribute = [NSFontAttributeName : UIFont.boldSystemFontOfSize(80)]
-
         if pageIndex == 1 {
-            numberAttributedString.addAttributes(greyColorAttribute, range: numberString.rangeOfString("2 3 4"))
+            setupTextView(pageBold1, forNormal: pageNormal1, inText: pageText1)
         } else if pageIndex == 2 {
-            numberAttributedString.addAttributes(greyColorAttribute, range: numberString.rangeOfString("1"))
-            numberAttributedString.addAttributes(greyColorAttribute, range: numberString.rangeOfString("3 4"))
+            setupTextView(pageBold2, forNormal: pageNormal2, inText: pageText2)
         } else if pageIndex == 3 {
-            numberAttributedString.addAttributes(greyColorAttribute, range: numberString.rangeOfString("1 2"))
-            numberAttributedString.addAttributes(greyColorAttribute, range: numberString.rangeOfString("4"))
+            setupTextView(pageBold3, forNormal: pageNormal3, inText: pageText3)
         } else if pageIndex == 4 {
-            numberAttributedString.addAttributes(greyColorAttribute, range: numberString.rangeOfString("1 2 3"))
+            setupTextView(pageBold4, forNormal: pageNormal4, inText: pageText4)
         }
-        numberAttributedString.addAttributes(boldAttribute, range: numberString.rangeOfString(numberString as String))
-        genericLabel.attributedText = numberAttributedString
+    }
+
+    private func setupTextView(forBold: String, forNormal: String, inText: String) {
+        let moreDetails = UILabel(frame: CGRectMake(view.frame.origin.x + 20, view.frame.origin.y + 110 , view.frame.size.width - 40, 100))
+        moreDetails.numberOfLines = 0
+        moreDetails.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        
+//        moreDetails.layer.borderWidth = 2
+//        moreDetails.layer.borderColor = UIColor.redColor().CGColor
+
+        let topBaseString = inText as NSString
+        let topAttributedString = NSMutableAttributedString(string: topBaseString as String)
+        let regularTopText = [NSFontAttributeName : UIFont.systemFontOfSize(30)]
+        let boldAttribute = [NSFontAttributeName : UIFont.boldSystemFontOfSize(30)]
+        topAttributedString.addAttributes(boldAttribute, range: topBaseString.rangeOfString(forBold))
+        topAttributedString.addAttributes(regularTopText, range: topBaseString.rangeOfString(forNormal))
+        moreDetails.attributedText = topAttributedString;
+
+        let subDetails = UILabel(frame: CGRectMake(moreDetails.frame.origin.x, view.frame.origin.y + moreDetails.frame.height + 50 , moreDetails.frame.size.width, 170))
+        let bottomString = "The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence."
+        subDetails.text = bottomString
+        subDetails.numberOfLines = 0
+        subDetails.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        subDetails.font = UIFont.systemFontOfSize(15)
+        
+//        subDetails.layer.borderWidth = 2
+//        subDetails.layer.borderColor = UIColor.blueColor().CGColor
+        
+        view.addSubview(subDetails)
+        view.addSubview(moreDetails)
     }
     
     
