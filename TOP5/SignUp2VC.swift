e52e8a14cd72f7a1502e88c29727c42af62d5410
john andrews <john.andrews@buzzfeed.com>
2015-10-25 17:57:28 -0400

    set up board

---
+++ TOP5/SignUp2VC.swift
@@ -0,0 +1,16 @@
+//
+//  SignUp2VC.swift
+//  TOP5
+//
+//  Created by John Andrews on 10/25/15.
+//  Copyright © 2015 John Andrews. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUp2VC: UIViewController {
+
+
+
+}
