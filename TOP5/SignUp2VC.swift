cb7c0d829b257d62f7f4675d1c7c24e5897ba916
iamjohnandrews <johnnydrews@gmail.com>
2015-10-25 22:06:20 -0400

    gotta add spacer views inbetween relationship buttons

--- TOP5/SignUp2VC.swift
+++ TOP5/SignUp2VC.swift
@@ -11,6 +11,79 @@ import UIKit
 
 class SignUp2VC: UIViewController {
 
+    @IBOutlet weak var tellMoreLabel: UILabel!
 
+    @IBOutlet weak var relationshipLabel: UILabel!
 
+    @IBOutlet weak var ethnicityLabel: UILabel!
+    
+    @IBOutlet weak var sexOLabel: UILabel!
+    
+    @IBOutlet weak var singleLabel: UIButton!
+    @IBOutlet weak var committedLabel: UIButton!
+    @IBOutlet weak var marriedLabel: UIButton!
+    @IBOutlet weak var divorcedLabel: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        
+        tellMoreLabel.preferredMaxLayoutWidth = self.view.bounds.width
+        relationshipLabel.preferredMaxLayoutWidth = self.view.bounds.width
+        ethnicityLabel.preferredMaxLayoutWidth = self.view.bounds.width
+        sexOLabel.preferredMaxLayoutWidth = self.view.bounds.width
+        
+        addAutoLayoutConstraints()
+    }
+    
+    func addAutoLayoutConstraints() {
+        tellMoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        relationshipLabel.translatesAutoresizingMaskIntoConstraints = false
+        ethnicityLabel.translatesAutoresizingMaskIntoConstraints = false
+        sexOLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let viewDict = ["tellMoreLabel": tellMoreLabel, "relationshipLabel": relationshipLabel, "ethnicityLabel": ethnicityLabel, "sexOLabel": sexOLabel]
+        
+        let tellMoreLabelHConstrinats = NSLayoutConstraint.constraintsWithVisualFormat("H:|[tellMoreLabel]|", options: .AlignAllCenterX, metrics: nil, views: viewDict)
+        self.view.addConstraints(tellMoreLabelHConstrinats)
+        
+        let relationshipLabelHConstrinats = NSLayoutConstraint.constraintsWithVisualFormat("H:|[relationshipLabel]|", options: .AlignAllCenterX, metrics: nil, views: viewDict)
+        self.view.addConstraints(relationshipLabelHConstrinats)
+        
+        let ethnicityLabelHConstrinats = NSLayoutConstraint.constraintsWithVisualFormat("H:|[ethnicityLabel]|", options: .AlignAllCenterX, metrics: nil, views: viewDict)
+        self.view.addConstraints(ethnicityLabelHConstrinats)
+        
+        let sexOLabelHConstrinats = NSLayoutConstraint.constraintsWithVisualFormat("H:|[sexOLabel]|", options: .AlignAllCenterX, metrics: nil, views: viewDict)
+        self.view.addConstraints(sexOLabelHConstrinats)
+        
+        let relationshipViewDict = ["singleLabel": singleLabel, "committedLabel": committedLabel, "marriedLabel": marriedLabel, "divorcedLabel": divorcedLabel]
+        
+        
+     
+        
+//        let vContraint = NSLayoutConstraint.constraintsWithVisualFormat("V:|-distanceFromNavBar-[tellMoreLabel]-60-[relationshipLabel]-vDistanceBTWLabels-[ethnicityLabel]-distanceFromNavBar-[sexOLabel]", options: NSLayoutFormatOptions.AlignAllLeft, metrics: metrics, views: viewDict)
+//        self.view.addConstraints(vContraint)
+        
+    }
+    
+    
+    // MARK: Actions
+    @IBAction func sexOButtonPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func ethnicityButtonPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func relationshipButtonPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func doneButtonPressed(sender: AnyObject) {
+    }
 }
